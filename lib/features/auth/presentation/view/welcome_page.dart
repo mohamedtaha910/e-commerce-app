7cd58f67d2d09a3cd51d060f4363bea17902e57f
mohamedtaha910 <mt164371@gmail.com>
2026-06-03 18:19:24 +0300

    edit welcome page

--- lib/features/auth/presentation/view/welcome_page.dart
+++ lib/features/auth/presentation/view/welcome_page.dart
@@ -8,128 +8,127 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      bottom: false,
-      child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.67,
-                  width: MediaQuery.of(context).size.width,
-                  child: ShaderMask(
-                    blendMode: BlendMode
-                        .srcIn, // Ensures the gradient only fills the SVG shape
-                    shaderCallback: (Rect bounds) {
-                      return LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          AppColors.primaryColor,
-                          AppColors.primaryColor.withAlpha(120),
-                          AppColors.primaryColor,
-                        ],
-                      ).createShader(bounds);
-                    },
-                    child: SvgPicture.asset(
-                      'assets/auth_images/header1.svg',
-                      // color: const Color.fromARGB(255, 198, 172, 151),
-                      // color: AppColors.secondaryColor.withAlpha(120),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 20,
-                  left: 15,
-                  right: 10,
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.685,
+                width: MediaQuery.of(context).size.width,
+                child: ShaderMask(
+                  blendMode: BlendMode
+                      .srcIn, // Ensures the gradient only fills the SVG shape
+                  shaderCallback: (Rect bounds) {
+                    return LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        const Color.fromARGB(255, 216, 94, 13),
+                        AppColors.primaryColor.withAlpha(180),
+                        const Color.fromARGB(255, 216, 94, 13),
+                      ],
+                    ).createShader(bounds);
+                  },
                   child: SvgPicture.asset(
-                    'assets/auth_images/login.svg',
+                    'assets/auth_images/header1.svg',
+                    // color: const Color.fromARGB(255, 198, 172, 151),
+                    // color: AppColors.secondaryColor.withAlpha(120),
                     fit: BoxFit.fill,
-                    height: MediaQuery.of(context).size.height * 0.48,
-                    // width: 130,
                   ),
                 ),
-                Positioned(
-                  top: 20,
-                  left: 12,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(2),
-                      decoration: BoxDecoration(
-                        // color: Colors.black38,
-                        color: Colors.grey.shade100,
-                        // color: AppColors.primaryColor.withAlpha(200),
-                        borderRadius: BorderRadius.circular(30),
-                        border: Border.all(color: Colors.black26, width: 0.4),
-                      ),
-                      child: Icon(
-                        Icons.chevron_left_rounded,
-                        color: Colors.black54,
-                        size: 28,
-                      ),
+              ),
+              Positioned(
+                top: 42,
+                left: 15,
+                right: 10,
+                child: SvgPicture.asset(
+                  'assets/auth_images/login.svg',
+                  fit: BoxFit.fill,
+                  height: MediaQuery.of(context).size.height * 0.48,
+                  // width: 130,
+                ),
+              ),
+              Positioned(
+                top: 42,
+                left: 16,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(color: Colors.black26, width: 0.4),
+                    ),
+                    child: Icon(
+                      Icons.chevron_left_rounded,
+                      color: Colors.black54,
+                      size: 28,
                     ),
                   ),
                 ),
-              ],
-            ),
-            Transform.translate(
-              offset: Offset(0, -28),
-
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: SvgPicture.asset('assets/auth_images/Welcome.svg'),
               ),
-            ),
-            // const SizedBox(height: 8),
-            Padding(
+            ],
+          ),
+          Transform.translate(
+            offset: Offset(0, -20),
+            child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                'Join thousands of happy shoppers. It’s fast, free, and only takes a few seconds!',
-                style: TextStyle(
-                  color: Colors.black45,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-            Spacer(),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  SvgPicture.asset('assets/auth_images/Welcome.svg'),
+                  const SizedBox(height: 32),
                   Text(
-                    'Continue',
+                    'Join thousands of happy shoppers. It’s fast, free, and only takes a few seconds!',
                     style: TextStyle(
                       color: Colors.black45,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
                     ),
-                  ),
-                  const SizedBox(width: 6),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage(),
-                        ),
-                      );
-                    },
-                    child: SvgPicture.asset('assets/auth_images/continue.svg'),
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 36),
-          ],
-        ),
+          ),
+
+          // const SizedBox(height: 8),
+          Spacer(),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Continue',
+                  style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(width: 6),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                  },
+                  child: SvgPicture.asset('assets/auth_images/continue.svg'),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 36),
+        ],
       ),
     );
   }
